f151acfa36539f83b094cd44d8f420f6812ddb2e
PatoVerde <leandrodenos@gmail.com>
2022-06-09 12:47:36 -0300

    Only nft

--- contracts/ohMyMinter.sol
+++ contracts/ohMyMinter.sol
@@ -2,19 +2,61 @@
 pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ohMyMinter is ERC721, Ownable {
-    using Counters for Counters.Counter;
-
+contract ohMyMinter is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;    
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyToken", "MTK") {}
+    address public minter;
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+    modifier onlyMinter {
+      require(msg.sender == minter || msg.sender == owner());
+      _;
+    }
+
+    constructor() ERC721("ohMyMinter", "OMM") {}
+
+    function safeMint(address to) public onlyMinter {
+      uint256 tokenId = _tokenIdCounter.current();
+      _tokenIdCounter.increment();
+      _safeMint(to, tokenId);
+    }
+
+    function setMinter(address _newMinter) public onlyMinter {
+      minter = _newMinter;
+    }
+
+    function tokenOfOwner(address _owner) public view returns(uint256[] memory){
+      uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
